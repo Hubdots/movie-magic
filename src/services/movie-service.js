8297cc717d75ffc61792c7ed3910663d9eefc2fc
Grozdan Valchev <160527635+Hubdots@users.noreply.github.com>
2025-02-12 19:36:11 +0200

    Update movie when editing

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -69,5 +69,8 @@ export default {
 
     delete(movieId) {
         return Movie.findByIdAndDelete(movieId);
+    },
+    update(movieId, movieData) {
+        return Movie.findByIdAndUpdate(movieId, movieData);
     }
 } 
