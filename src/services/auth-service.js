b22dd5582e80f67cda00b5c811358136b6afe744
Grozdan Valchev <160527635+Hubdots@users.noreply.github.com>
2025-02-13 20:47:24 +0200

    Add user validations

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,7 +5,18 @@ import jwt from 'jsonwebtoken';
 const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
 export default {
-    register(userData) {
+    async register(userData) {
+
+        if (userData.password !== userData.rePassword) {
+            throw new Error('Password mismatch!');
+        };
+        
+        const userCount = await User.countDocuments({ filter: userData.email });
+
+        if (userCount > 0) {
+            throw new Error('This email already exists!')
+        };
+
         return User.create(userData);
     },
     async login(email, password) {
